edd255ad92f6b9149a0bcc7fa21f6ac6822c179f
Batman Mac <brianjorda@gmail.com>
2024-09-01 16:06:35 -0300

    handle movie

--- src/server.mjs
+++ src/server.mjs
@@ -1,28 +1,43 @@
 import express from "express";
 import { SERVERS } from "./constants/servers.js";
-import parseVidsrcMe from "./utils/parseVidsrcMe.js";
+import parseSourcesWithSubtitles from "./utils/parseSourcesWithSubtitles.js";
 import handleMovie from "./utils/handleMovie.js";
+import session from "express-session";
+import dotenv from "dotenv";
+// import { handleFetchSubtitlesByMovie, loginOpenSubtitles } from "./sources/opensubtitles.js";
 
+const env = dotenv.config().parsed;
 const app = express();
 const port = 3030;
 
+app.use(session({
+    secret: env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+}));
+
 app.get('/', async (req, res) => {
+    // if (!req.session.isLoggedIn) {
+    //     await loginOpenSubtitles(req);
+    // }
+    console.log({isLoggedIn: req.session.isLoggedIn, os_token: req.session.os_token});
     res.send('Welcome to my server!');
 });
 
-app.get('/movie/vidsrc/:movieId', async ({ params }, res) => {
+app.get('/movie/vidsrc/:movieId', async ({ params, ...req }, res) => {
     const { movieId } = params;
     handleMovie(movieId, SERVERS.VIDSRC, res);
+    // await handleFetchSubtitlesByMovie(movieId, req);
 });
 
 app.get('/movie/vidsrcme/:movieId', async ({ params }, res) => {
     const { movieId } = params;
-    handleMovie(movieId, SERVERS.VIDSRCME, res, parseVidsrcMe);
+    handleMovie(movieId, SERVERS.VIDSRCME, res, parseSourcesWithSubtitles);
 });
 
 app.get('/movie/flixhq/:movieId', async ({ params }, res) => {
     const { movieId } = params;
-    handleMovie(movieId, SERVERS.FLIXHQ, res);
+    handleMovie(movieId, SERVERS.FLIXHQ, res, parseSourcesWithSubtitles);
 });
 
 app.get('/movie/myflixerz/:movieId', async ({ params }, res) => {
